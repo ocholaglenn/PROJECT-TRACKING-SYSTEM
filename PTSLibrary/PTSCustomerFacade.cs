6c1dd9d392ea4da40b020f0197e195885e683440
ocholaglenn <107888460+ocholaglenn@users.noreply.github.com>
2022-08-07 21:18:11 +0300

    Add files via upload
    A Guided Project Tracking System of 5 collaborators
    Goal: Building a distributed information system with heterogeneous clients
    Project Structure (7-Phases)
    1.Introduction to Distributed System & Database creation [DONE]
    2. Building a business component using C# [DONE]
    3. Building an admin GUI using C#
    4. Introduction to .NET remoting (implement a remote server application))
    5. Create a web service and client website
    6. Develop a java client
    7. Use creativity to improve on the solution we build

---
+++ PTSLibrary/PTSCustomerFacade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSLibrary
+{
+    public class PTSCustomerFacade:PTSSuperFacade
+    {
+        protected DAO.CustomerDAO dao;
+        public PTSCustomerFacade() : base(new DAO.CustomerDAO())
+        {
+            dao = (DAO.CustomerDAO)base.dao;
+        }
+        public Project[] GetListOfProjects(int customerId)
+        {
+            return (dao.GetListOfProjects(customerId)).ToArray();
+        }
+        public int Authenticate(string username, string password)
+        {
+            if (username == "" || password == "")
+            {
+                throw new Exception("Missing Data");
+            }
+            return dao.Authenticate(username, password);
+        }
+    }
+}
